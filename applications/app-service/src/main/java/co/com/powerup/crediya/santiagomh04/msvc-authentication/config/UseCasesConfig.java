e552c5ce9464d4de7ebfbcfe7d031e5ca5966d2f
Santiago <santimh04@gmail.com>
2025-09-03 13:00:48 -0500

    feat: Scaffold Bancolombia structure defined to create the project

---
+++ applications/app-service/src/main/java/co/com/powerup/crediya/santiagomh04/msvc-authentication/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.powerup.crediya.santiagomh04.msvc-authentication.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.powerup.crediya.santiagomh04.msvc-authentication.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
