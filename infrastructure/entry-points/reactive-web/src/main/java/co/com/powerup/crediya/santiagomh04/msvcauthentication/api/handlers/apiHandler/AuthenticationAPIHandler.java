30a65a9f6ba07a1296a41b5aaed968eff498cd0b
Santiago <santimh04@gmail.com>
2025-10-17 16:35:41 -0500

    feat: user credentials for login endpoint properly validated

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/crediya/santiagomh04/msvcauthentication/api/handlers/apiHandler/AuthenticationAPIHandler.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/powerup/crediya/santiagomh04/msvcauthentication/api/handlers/apiHandler/AuthenticationAPIHandler.java
@@ -3,6 +3,9 @@ package co.com.powerup.crediya.santiagomh04.msvcauthentication.api.handlers.apiH
 import co.com.powerup.crediya.santiagomh04.msvcauthentication.api.dto.UserCredentialsRequestDTO;
 import co.com.powerup.crediya.santiagomh04.msvcauthentication.api.mappers.UserCredentialsMapper;
 import co.com.powerup.crediya.santiagomh04.msvcauthentication.usecase.authentication.AuthenticationUseCase;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
@@ -11,6 +14,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
@@ -18,20 +22,20 @@ public class AuthenticationAPIHandler {
 
     private final AuthenticationUseCase authenticationUseCase;
 
-    /*private final Validator validator;*/
+    private final Validator validator;
 
     private final UserCredentialsMapper userCredentialsMapper;
 
     public Mono<ServerResponse> listenLoginUseCase(ServerRequest serverRequest) {
         URI uri = serverRequest.uri();
         return serverRequest.bodyToMono(UserCredentialsRequestDTO.class)
-            /*.flatMap(loginRequest -> {
-                Set<ConstraintViolation<LoginRequest>> violations = validator.validate(loginRequest);
+            .flatMap(loginRequest -> {
+                Set<ConstraintViolation<UserCredentialsRequestDTO>> violations = this.validator.validate(loginRequest);
                 if (!violations.isEmpty()) {
                     return Mono.error(new ConstraintViolationException(violations));
                 }
                 return Mono.just(loginRequest);
-            })*/
+            })
             .map(this.userCredentialsMapper::toDomain)
             .flatMap(this.authenticationUseCase::login)
             .map(this.userCredentialsMapper::toResponse)
